cb1fbc9e84e31e57a704b511b1720b7f74d8a2c9
Cohabitat Coworking <contato@cohabitat.com.br>
2017-09-01 12:12:42 -0300

    Hey

---
+++ js/main.js
@@ -0,0 +1,99 @@
+// Formulario
+
+$('.formphp').on('submit', function() {
+	var emailContato = "contato@cohabitat.com.br"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Logo entraremos em contato com você.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
+
+//Slide
+   
+$(function () {
+
+  // Slideshow 1
+  $("#slider1").responsiveSlides({
+    auto: false,
+    pager: true,
+    nav: true,
+    speed: 500,
+    maxwidth: 800,
+    namespace: "centered-btns"
+  });
+});
+
+
+//Mobile Plugin
+
+$('.header ul').origamidMenu({
+	breakpoint: 959,
+	top: 52,
+	background: "#588276",
+	color: "#b0c268"
+});
+
+//Animate
+
+('.menu-nav a[href^="#"]').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.header').innerHeight(),
+			targetOffset = $(id).offset().top;
+	
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
+});
+
